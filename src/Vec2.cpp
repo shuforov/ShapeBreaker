36363202fa3a6dd6456c1ff5846f8690f0252ab7
shuforov <shuforov@gmail.com>
2025-07-09 15:21:08 +0300

    add logic to all functions of Vec2 class.

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,59 +1,64 @@
 #include "../include/Vec2.h"
+#include <cmath>
 #include <math.h>
 
 Vec2::Vec2() {
 }
 
-
 Vec2::Vec2(float xin, float yin) : x(xin), y(yin) {}
 
 Vec2 Vec2::operator + (const Vec2& rhs) const {
-  // TODO
-  return Vec2(0, 0);
+  return Vec2(x + rhs.x, y + rhs.y);
 }
 
 Vec2 Vec2::operator - (const Vec2& rhs) const {
-  // TODO
-  return Vec2(0,0);
+  return Vec2(x - rhs.x, y - rhs.y);
 }
 
 Vec2 Vec2::operator / (const float val) const {
-  // TODO
-  return Vec2(0, 0);
+  return Vec2(x / val, y / val);
 }
 
 Vec2 Vec2::operator * (const float val) const {
-  // TODO
-  return Vec2(0, 0);
+  return Vec2(x * val, y * val);
 }
 
 bool Vec2::operator == (const Vec2& rhs) const {
-  // TODO
+  if (x == rhs.x && y == rhs.y) {
+    return true;
+  }
   return false;
 }
 
 bool Vec2::operator != (const Vec2& rhs) const {
-  // TODO
+  if (x != rhs.x || y != rhs.y) {
+    return true;
+  }
   return false;
 }
 
 void Vec2::operator += (const Vec2& rhs) {
-  // TODO
+  x += rhs.x;
+  y += rhs.y;
 }
 
 void Vec2::operator -= (const Vec2& rhs) {
-  // TODO
+  x -= rhs.x;
+  y -= rhs.y;
 }
 
 void Vec2::operator *= (const float val) {
-  // TODO
+  x *= val;
+  y *= val;
 }
 
 void Vec2::operator /= (const float val) {
-  // TODO
+  x /= val;
+  y /= val;
 }
 
 float Vec2::dist(const Vec2& rhs) const {
-  // TODO
-  return 0;
+  float dx = rhs.x - x;
+  float dy = rhs.y - y;
+  return std::sqrt((dx * dx) + (dy * dy));
 }
